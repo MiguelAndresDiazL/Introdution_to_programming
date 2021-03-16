c433ae1b3d50040c0a33f947496c3d095c7c615c
Miguel Andres Diaz <miguel.diazl@usantoto.edu.co>
2021-03-16 13:41:09 -0500

    Excercises in JAVA
    Excercises in JAVA

---
+++ Excercises in JAVA/P1T34_JAVA_HELLO_WORLD/src/usta/fis/Main.java
@@ -0,0 +1,19 @@
+package usta.fis;
+
+//AUTHOR: Miguel Andres Diaz
+//DATE: 2021-March-16
+//DESCRIPTION: This is my first program in JAVA
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("-----------------------------");
+        System.out.println("softhelloworld version 1.0   ");
+        System.out.println("Maker: Miguel Andres Diaz  ");
+        System.out.println("DESCRIPTION: this software show HELLOW WORLD");
+        System.out.println("-----------------------------");
+
+        System.out.println("hello world");
+    }
+}
---
+++ Excercises in JAVA/P1T35_JAVA_INPUT_NUMBER/src/usta/fis/Main.java
@@ -0,0 +1,27 @@
+package usta.fis;
+
+//AUTHOR: Miguel Andres Diaz
+//DATE: 2021-march-16
+//DESCRIPTION: this software show variable than input by keyboard
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("-----------------------");
+        System.out.println("SoftInputNum version 1.0");
+        System.out.println("Maker: Miguel Andres Diaz");
+        System.out.println("-----------------------");
+        Scanner keyboard = new Scanner(System.in);
+
+        int v_number1, v_number2, v_sum;
+        System.out.println("Input the first number:");
+        v_number1 = keyboard.nextInt();
+        System.out.println("Input the second number:");
+        v_number2 = keyboard.nextInt();
+        v_sum = v_number1 + v_number2;
+        System.out.println("The total sum is: "+ v_sum);
+
+    }
+}
