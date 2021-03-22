b60d22f74e77c0bc696ee6d384ff66c4b32b24f8
Miguel Andres Diaz <miguel.diazl@usantoto.edu.co>
2021-03-22 11:05:45 -0500

    Exercises in JAVA
    Exercises in JAVA

---
+++ Excercises in JAVA/P1T36_JAVA_System-out-print/src/usta/fis/Main.java
@@ -0,0 +1,39 @@
+package usta.fis;
+
+//AUTHOR: Miguel Andres Diaz
+//DATE: 2021-march-19
+//DESCRIPTION: this software display an original figure on the screen and along with the name of each one in relief
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("             $$$$\n" +
+                "           $$$$$$$$$\n" +
+                "           $$$$$$_$\n" +
+                "          $$$$$$$$$\n" +
+                "           $$$$$$$$\n" +
+                "            $$$$$$$$\n" +
+                "           $$$$$$$$$$$\n" +
+                "          $$$$$$_$$$$$\n" +
+                "          $$$$$$    $$$\n" +
+                "          $$$$$$$$   $\n" +
+                "           $$$$$$$$$\n" +
+                "            $$$$$$$$$\n" +
+                "            $$$$$$$$$$\n" +
+                "             $$$$$$$$$$\n" +
+                "        $$$$__$$$$$$$$$\n" +
+                "            $$$$$$$$$$$\n" +
+                "              $$$$$$$$$\n" +
+                "           $$$$$$$$$$$$$\n" +
+                "               $$$$$$$$$\n" +
+                "               $$$$$$$$$\n" +
+                "               $$$$$$$$        $\n" +
+                "                $$$$$$$       $$ $$\n" +
+                "                 $$$$$$       $  $$\n" +
+                "                  $$$$          $$\n" +
+                "                   $$$         $$$\n" +
+                "                   $$$$$$$$$$$$$$$\n" +
+                "                     $$$$$$$$$$\n" +
+                "");
+    }
+}
