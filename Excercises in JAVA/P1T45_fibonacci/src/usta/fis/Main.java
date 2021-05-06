9ea8dd6e975f519ccc155cb87bc7ac5a448aba05
Miguel Andres Diaz <miguel.diazl@usantoto.edu.co>
2021-05-06 07:06:20 -0500

    Works in java
    Works in java

--- Excercises in JAVA/P1T45_fibonacci/src/usta/fis/Main.java
+++ Excercises in JAVA/P1T45_fibonacci/src/usta/fis/Main.java
@@ -1,33 +1,39 @@
 package usta.fis;
 
 import java.util.Scanner;
-public class Main {
 
+public class Main {
     public static void main(String[] args) {
         p_show_info_program();
-        int serie=0;
+        int i = 0;
+        int serie = 0;
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Input your limit numbers for the fabonacci series: ");
+        System.out.println("Ingresa el numero limites de la escala fibonacci: ");
         serie = keyboard.nextInt();
         int fibo_array[] = new int[100];
         fibo_array = serie_fibonacci(serie);
-        System.out.println("the fabonacci series for number" + serie + "its:");
-        for (int i = 0; i < serie; i++) {
-            if (fibo_array[i] > 0) {
-                System.out.println(fibo_array[i] + ", ");
+        System.out.println("la escala fibonacci para el numero "+serie+" es:");
+        while (fibo_array[i] != -1) {
+            if (fibo_array[i+1]!=-1) {
+                System.out.print(fibo_array[i] + ", ");
+            }else{
+                System.out.print(fibo_array[i]);
             }
+            i++;
         }
     }
 
-     public static int [] serie_fibonacci(int limit) {
+    public static int [] serie_fibonacci(int limit) {
         int fibo_array[]= new int[100];
+        int i = 1;
         fibo_array[0]=0;
         fibo_array[1]=1;
-         for (int i = 1; (fibo_array[i-1]+fibo_array[i]) < limit; i++) {
-         fibo_array[i+1] = fibo_array[i-1] + fibo_array[i];
-         fibo_array[i+2] = -1;
-         }return fibo_array;
-     }
+        while ((fibo_array[i-1]+fibo_array[i]) < limit) {
+            fibo_array[i+1] = fibo_array[i-1] + fibo_array[i];
+            fibo_array[i+2] = -1;
+            i++;
+        }return fibo_array;
+    }
     public static void p_show_info_program () {
         System.out.println("╔═════════════════════════════╗");
         System.out.println("║      fabonacci-soft V1.0    ║");
@@ -35,5 +41,6 @@ public class Main {
         System.out.println("║   Date: 2021-April-26       ║");
         System.out.println("╚═════════════════════════════╝");
     }
-    }
+}
+
 
